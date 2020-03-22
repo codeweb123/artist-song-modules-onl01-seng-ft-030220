c02d04936cb19c0d75e63308964f9b9fa9813a85
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 02:25:35 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,10 @@
 module Memorable
-  
+
   module Song
     def self.reset_all
       self.all.clear
     end
-    
+
     def self.count
       self.all.count
     end
@@ -13,7 +13,7 @@ module Memorable
     def self.reset_all
       self.all.clear
     end
-    
+
     def self.count
       self.all.count
     end
