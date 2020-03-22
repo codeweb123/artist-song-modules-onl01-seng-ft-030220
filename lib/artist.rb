8da8c24bcfbe526fc0deb635ccfa86fb955a357a
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 02:57:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,31 @@
+require 'pry'
+
 class Artist
   attr_accessor :name
   attr_reader :songs
-
   extend Memorable::ClassMethods
-  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
+  extend Findable
+  include Paramable
 
   @@artists = []
-
-  def self.all
-    @@artists
-  end
 
   def initialize
     super
     @songs = []
   end
 
+  def self.all
+    @@artists
+  end
+
   def add_song(song)
     @songs << song
-    song.artist = self unless song.artist
+    song.artist = self
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+
 end
