a7925f9d549f62d219810fc229c77abc82690d5b
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 02:57:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,20 @@
+require 'pry'
+
 class Song
-
   extend Memorable::ClassMethods
-  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
+  def artist=(artist)
+    @artist = artist
+  end
+
   def self.all
     @@songs
   end
-
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self) unless artist.songs.include?(self)
-  end
-
 end
